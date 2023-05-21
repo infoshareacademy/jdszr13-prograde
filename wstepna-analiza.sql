892f723746e6dbb5a95cf59125a9d6e3ef5341c2
Mikolaj <mik5owski@op.pl>
2023-05-21 11:40:17 +0200

    updated wstepna-analiza with passangers share of each domestic airline

--- wstepna-analiza.sql
+++ wstepna-analiza.sql
@@ -139,3 +139,41 @@ select sum("PASSENGERS TO INDIA") , "AIRLINE NAME"  from airlinewise a
 --where "AIRLINE NAME" = 'JET AIRWAYS' or "AIRLINE NAME" = 'AIR INDIA' or "AIRLINE NAME" = 'AIR INDIA EXPRESS'
 group by "AIRLINE NAME"
 order by sum("PASSENGERS TO INDIA") desc
+----------------------------------------------------------------------------------------------------
+
+--porównanie wielkości udziału procentowego w przewozach pasażerskich w 
+--kolejnych latach w zależności od przewoźnika
+
+
+select  "AIRLINE NAME" ,"YEAR"::text , 
+sum("PASSENGERS TO INDIA" + "PASSENGERS FROM INDIA") Passengers,
+round(sum("PASSENGERS TO INDIA" + "PASSENGERS FROM INDIA")::numeric /
+(select sum("PASSENGERS TO INDIA" + "PASSENGERS FROM INDIA")
+  from airlinewise a2 
+  where "CARRIER TYPE" = 'DOMESTIC' and  "YEAR" = '2015')*100,2)share_Passengers
+from airlinewise a 
+where "CARRIER TYPE" = 'DOMESTIC' and  "YEAR" = '2015'
+group by "AIRLINE NAME" ,"YEAR" 
+union
+select  "AIRLINE NAME" ,"YEAR"::text , 
+sum("PASSENGERS TO INDIA" + "PASSENGERS FROM INDIA") Passengers,
+round(sum("PASSENGERS TO INDIA" + "PASSENGERS FROM INDIA")::numeric /
+(select sum("PASSENGERS TO INDIA" + "PASSENGERS FROM INDIA")
+  from airlinewise a2 
+  where "CARRIER TYPE" = 'DOMESTIC' and  "YEAR" = '2016')*100,2) share_Passengers
+from airlinewise a 
+where "CARRIER TYPE" = 'DOMESTIC' and  "YEAR" = '2016'
+group by "AIRLINE NAME" ,"YEAR" 
+union
+select  "AIRLINE NAME" ,"YEAR"::text , 
+sum("PASSENGERS TO INDIA" + "PASSENGERS FROM INDIA") Passengers,
+round(sum("PASSENGERS TO INDIA" + "PASSENGERS FROM INDIA")::numeric /
+(select sum("PASSENGERS TO INDIA" + "PASSENGERS FROM INDIA")
+  from airlinewise a2 
+  where "CARRIER TYPE" = 'DOMESTIC' and  "YEAR" = '2017')*100,2) share_Passengers
+from airlinewise a 
+where "CARRIER TYPE" = 'DOMESTIC' and  "YEAR" = '2017'
+group by "AIRLINE NAME" ,"YEAR"  
+order by "AIRLINE NAME", "YEAR" DESC
+--duże firmy przewozowe tracą procentowy udział w rynku na rzecz małych
+--przewoźników
