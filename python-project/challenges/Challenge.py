d830fe1c88411db161f3c4e8cfbede4bc35fdebe
Mikolaj <mik5owski@op.pl>
2023-07-11 21:53:22 +0200

    tested class Challenge on my team and added atribute to clear scores

--- python-project/challenges/Challenge.py
+++ python-project/challenges/Challenge.py
@@ -3,7 +3,7 @@ class Challenge:
     # player_pokemons = {} this should count points for each pokemon
     total_wins = 0
     total_iterations = 0
-    
+
     def __init__(self, player_team, possible_opponents, type_of_challange):
         self.player_team = player_team.reset_index()
         self.possible_opponents = possible_opponents
@@ -16,17 +16,20 @@ class Challenge:
         # add pokemons to player_pokemons dictionary
         return
     
-    def perform_challenge(self):
+    def clear_scores(self):
+        self.total_iterations = 0
+        self.total_wins = 0
 
+    def perform_challenge(self):
+        self.clear_scores()
         for i in range(len(self.player_team)):
             for _ in range(100):
                 opponent = self.choose_random_opponent()
-
                 if self.player_team[self.type_of_challange].iloc[i] > opponent[self.type_of_challange].iloc[0]:
                     self.total_wins += 1
-            total_iterations += 1
-        return self.total_wins
-
+                self.total_iterations += 1
+        return (self.total_wins, self.total_iterations)
+    
     # save_results
 
     # show_results
