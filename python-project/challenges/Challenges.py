925ee3df9243b69a5149c3a29c0be41deb28cc3e
Mikolaj <mik5owski@op.pl>
2023-08-23 10:51:39 +0200

    added changes to Challenges file

--- python-project/challenges/Challenges.py
+++ python-project/challenges/Challenges.py
@@ -21,7 +21,7 @@ class Challenges:
         self.possible_opponents = possible_opponents
 
     def choose_random_opponent(self):
-        return self.possible_opponents.sample(1)
+        return self.possible_opponents.sample(1) 
     
     def create_pokemon_individual_score_table(self):
         self.pokemon_score = {}
@@ -52,6 +52,7 @@ class Challenges:
                 if self.player_team[challenge_type].iloc[i] > opponent[challenge_type].iloc[0]:
                     self.increase_current_score()
                     self.increase_pokemon_individual_score(i)
+                    
         
         return (self.current_challenge_wins, self.pokemon_score)
     
@@ -59,35 +60,11 @@ class Challenges:
         for challenge_type in self.challenge_types:
             score = self.perform_challenge(challenge_type)
             self.total_wins += score[0]
-            print(score)
-            # print(self.scores_from_all_challenges)
             self.scores_from_all_challenges[challenge_type].update(score[1]) 
             
+        print(self.scores_from_all_challenges)
         percentage_of_wins = int(self.total_wins/3600*100)
         print(f'Out of possible 3600 wins, team {self.team_name} achieved: {self.total_wins}. That is {percentage_of_wins}%.')
         self.clear_total_score()
-        self.draw_plots()
-
-    def draw_plots(self):
-        plt.clf()
-        fig, axs = plt.subplots(2,3, figsize=(20,10))
-        x = self.scores_from_all_challenges['attack'].keys()
-
-
-        axs[0, 0].bar(x, self.scores_from_all_challenges['attack'].values())
-        axs[0, 0].set_title('Attack Challenge')
-        axs[0, 1].bar(x, self.scores_from_all_challenges['defense'].values())
-        axs[0, 1].set_title('Defense Challenge')
-        axs[0, 2].bar(x, self.scores_from_all_challenges['hp'].values())
-        axs[0, 2].set_title('Endurance Challenge')
-        axs[1, 0].bar(x, self.scores_from_all_challenges['sp_attack'].values())
-        axs[1, 0].set_title('Special Attack Challenge')
-        axs[1, 1].bar(x, self.scores_from_all_challenges['sp_defense'].values())
-        axs[1, 1].set_title('Special Attack Challenge')
-        axs[1, 2].bar(x, self.scores_from_all_challenges['speed'].values())
-        axs[1, 2].set_title('Speed Challenge')
-
-        fig.suptitle(f'Team {self.team_name} Results')
-        plt.show()
 
 
